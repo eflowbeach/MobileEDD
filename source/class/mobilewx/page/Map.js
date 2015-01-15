2340f61584511b7b9039aa9e17a18d126be35309
jonathan.wolfe <jonathan.wolfe@noaa.gov>
2015-01-15 12:47:44 -0500

    Updated some css setting to remove stripe and testing color scheme.

--- source/class/mobilewx/page/Map.js
+++ source/class/mobilewx/page/Map.js
@@ -86,10 +86,26 @@ qx.Class.define("mobilewx.page.Map",
           })],
           view : new ol.View(
           {
-            center : ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
+            center : ol.proj.transform([ -99,40], 'EPSG:4326', 'EPSG:3857'),
             zoom : 4
           })
         });
+
+        var deviceOrientation = new ol.DeviceOrientation();
+        // tilt the map
+        deviceOrientation.on(['change:beta', 'change:gamma'], function(event) {
+          var center = view.getCenter();
+          var resolution = view.getResolution();
+          var beta = event.target.getBeta() || 0;
+          var gamma = event.target.getGamma() || 0;
+
+          center[0] -= resolution * gamma * 25;
+          center[1] += resolution * beta * 25;
+
+          view.setCenter(view.constrainCenter(center));
+        });
+
+
       }.bind(this);
       req.open("GET", this._mapUri);
       req.send();
